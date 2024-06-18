ffc94054829c7df41c25759ca364b958a3b432d9
SMA <sma@wizlinx.com>
2024-06-19 01:41:51 +0500

    additional routes

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -7,7 +7,7 @@ import {
 } from "@nextui-org/react";
 import Link from "next/link";
 import React from "react";
-import { GiMatchTip } from "react-icons/gi";
+import { RiLinksFill } from "react-icons/ri";
 
 export default function TopNav() {
   return (
@@ -19,10 +19,10 @@ export default function TopNav() {
       }}
     >
       <NavbarBrand as={Link} href="/">
-        <GiMatchTip size={40} className="text-gray-200" />
+        <RiLinksFill size={40} className="text-gray-100" />
         <div className="font-bold text-3xl flex">
-          <span className="text-gray-900">Link</span>
-          <span className="text-gray-200">Up</span>
+          <span className="text-gray-700">Link</span>
+          <span className="text-gray-100">Up</span>
         </div>
       </NavbarBrand>
       <NavbarContent justify="center">
@@ -37,10 +37,20 @@ export default function TopNav() {
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <Button variant="bordered" className="text-white">
+        <Button
+          as={Link}
+          href="/login"
+          variant="bordered"
+          className="text-white"
+        >
           Login
         </Button>
-        <Button variant="bordered" className="text-white">
+        <Button
+          as={Link}
+          href="/register"
+          variant="bordered"
+          className="text-white"
+        >
           Register
         </Button>
       </NavbarContent>
