786ca0d18632bba771fb30413918a0442a3f420f
SMA <sma@wizlinx.com>
2024-06-19 05:05:11 +0500

    React hook forms

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,8 +1,21 @@
+"use client";
+
 import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { GiPadlock } from "react-icons/gi";
 
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    console.log(errors.email);
+  };
   return (
     <Card className="w-2/5 mx-auto">
       <CardHeader className="flex flex-col items-center justify-center">
@@ -15,11 +28,34 @@ export default function LoginForm() {
         </div>
       </CardHeader>
       <CardBody>
-        <form action="">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="space-y-4">
-            <Input label="Email" variant="bordered" />
-            <Input label="Password" variant="bordered" type="password" />
-            <Button fullWidth color="secondary" type="submit">
+            <Input
+              defaultValue=""
+              label="Email"
+              variant="bordered"
+              {...register("email", { required: "Email Address is required" })}
+              isInvalid={errors.email ? true : false}
+              errorMessage={errors.email?.message as string}
+            />
+            <Input
+              defaultValue=""
+              label="Password"
+              variant="bordered"
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+              })}
+              isInvalid={errors.password ? true : false}
+              errorMessage={errors.password?.message as string}
+            />
+
+            <Button
+              isDisabled={!isValid}
+              fullWidth
+              color="secondary"
+              type="submit"
+            >
               Login
             </Button>
           </div>
